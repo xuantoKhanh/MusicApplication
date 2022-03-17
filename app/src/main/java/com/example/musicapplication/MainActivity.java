01184455dfe87157d1cc21afc890880ed9b677b3
Admin <tokhanhxuan99@gmail.com>
2022-03-17 17:26:42 +0700

    foreground notification nhung chua hien video

--- app/src/main/java/com/example/musicapplication/MainActivity.java
+++ app/src/main/java/com/example/musicapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.musicapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.exoplayer2.ExoPlayer;
@@ -13,29 +15,13 @@ import com.google.android.exoplayer2.ui.StyledPlayerView;
 
 public class MainActivity extends AppCompatActivity {
 
-    String url1 = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
-    String url2 = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4";
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ExoPlayer player = new ExoPlayer.Builder(this).build();
-        PlayerView playerControlView = findViewById(R.id.exoPlayer);
-        // Bind the player to the view.
-        playerControlView.setPlayer(player);
-
-        // Build the media item.
-        MediaItem mediaItem1 = MediaItem.fromUri(url1);
-        MediaItem mediaItem2 = MediaItem.fromUri(url2);
-// Set the media item to be played.
-        player.addMediaItem(mediaItem1);
-        player.addMediaItem(mediaItem2);
-// Prepare the player.
-        player.prepare();
-// Start the playback.
-        player.play();
+        Intent serviceIntent = new Intent(this, PlayerNotificationService.class);
+        serviceIntent.putExtra("inputExtra", "Foreground Service Example in Android");
+        ContextCompat.startForegroundService(this, serviceIntent);
     }
 }
---
+++ app/src/main/java/com/example/musicapplication/PlayerNotificationService.java
@@ -0,0 +1,108 @@
+package com.example.musicapplication;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.ui.PlayerNotificationManager;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.util.Util;
+
+import java.security.Provider;
+
+public class PlayerNotificationService extends Service {
+
+    public static final String CHANNEL_ID = "ForegroundServiceChannel";
+
+    String url1 = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+    String url2 = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4";
+    String url3 = " http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4";
+
+    private SimpleExoPlayer mPlayer;
+    private PlayerView playerControlView;
+    private DataSource.Factory dataSourceFactory;
+    private PlayerNotificationManager playerNotificationManager;
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+//        ExoPlayer player = new ExoPlayer.Builder(this).build();
+//        playerControlView = playerControlView.findViewById(R.id.exoPlayer);
+//        // Bind the player to the view.
+//        playerControlView.setPlayer(player);
+//
+//        // Build the media item.
+//        MediaItem mediaItem1 = MediaItem.fromUri(url1);
+//        MediaItem mediaItem2 = MediaItem.fromUri(url2);
+//        MediaItem mediaItem3 = MediaItem.fromUri(url3);
+//// Set the media item to be played.
+//        player.addMediaItem(mediaItem1);
+//        player.addMediaItem(mediaItem2);
+//        player.addMediaItem(mediaItem3);
+//// Prepare the player.
+//        player.prepare();
+//// Start the playback.
+//        player.play();
+
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        String input = intent.getStringExtra("inputExtra");
+        createNotificationChannel();
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                0, notificationIntent, 0);
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Foreground Service")
+                .setContentText(input)
+                .setSmallIcon(R.drawable.music_video)
+                .setContentIntent(pendingIntent)
+                .build();
+        startForeground(1, notification);
+        //do heavy work on a background thread
+        //stopSelf();
+
+        return START_NOT_STICKY;
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel serviceChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Foreground Service Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(serviceChannel);
+        }
+    }
+
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
